f4bf52d355990174f12182e4e58edcf716935bcf
kujiKazuaki <kuji32global@gmail.com>
2024-04-06 23:11:59 +0900

    フォルダ展開

--- main.js
+++ main.js
@@ -1,24 +1,68 @@
-import "./plugins/todoList.js";
-import { display_header_page } from "./plugins/header.js";
+const mainBtn = document.getElementById("favorite-btn")
+const favoArea = document.getElementById('favorite-word');
+const already_List = document.getElementById("already-List")
+const selected_num = document.getElementById("selected-favorite-word")
 
-display_header_page("C:/Users/kazuaki.kuji/Desktop/研修/sample/profile/modules/header.html", "header");
+/**
+ * 画面ロード時、発掘した好きな言葉リストを出現させる関数
+ */
+document.addEventListener('DOMContentLoaded', () => {
+    const favoWords = localStorage.getItem('selectedFavoWord');
+    const alreadyNum = localStorage.getItem("already_selected_num")
 
+    if (favoWords) {
+        const favoWordsArray = JSON.parse(favoWords);
+        if (favoWordsArray.length > 0) {
+            already_List.innerHTML = favoWordsArray.map(word => `<li>${word}</li>`).join('');
+            selected_num.textContent = alreadyNum || favoWordsArray.length;
+        }
+    }
+})
 
-const button1 = document.querySelector(".button1")
-const button2 = document.querySelector(".button2")
-const result = document.querySelector('.program1_result');
+/**
+ * 好きなことばのボタンを押した際、好きな言葉リストからランダムに出力する関数
+ */
+const pushFavoWord = () => {
+    const favoWord_List = [
+        "苦あれば楽あり。楽あれば苦あり",
+        "井の中の蛙大海を知らず",
+    ]
+    const randomIndex = Math.floor(Math.random() * favoWord_List.length);
+    const selectedFavoWord = favoWord_List[randomIndex];
 
-let count = 0;
+    // idが'pushZone'の要素に選択されたアイテムを表示
+    if (favoArea) {
+        favoArea.textContent = ""
+        favoArea.textContent = selectedFavoWord;
+    } else {
+        throw Error("エラー")
+    }
 
-function countUp() {
-    count += 1;
-    result.textContent = count
+    // ローカルストレージからselectedFavoWordリストを取得または作成
+    let favoWords = localStorage.getItem('selectedFavoWord');
+    favoWords = favoWords ? JSON.parse(favoWords) : [];
+
+    // 選択されたアイテムがリストになければ追加
+    if (!favoWords.includes(selectedFavoWord)) {
+        /**
+         * 発掘した言葉をリストに追加する
+         * 発掘した好きな言葉カウントを１つカウントアップする
+         */
+        const card = document.createElement("div")
+        card.className = "card"
+        card.textContent = selectedFavoWord
+        already_List.appendChild(card)
+
+        let already_selected_num = Number(selected_num.textContent)
+        already_selected_num++
+        selected_num.textContent = already_selected_num
+        localStorage.setItem("already_selected_num", already_selected_num)
+        /**
+         * JSONに保存してリロードしても保管できるようにする
+         */
+        favoWords.push(selectedFavoWord);
+        localStorage.setItem('selectedFavoWord', JSON.stringify(favoWords));
+    }
 }
 
-function countDown() {
-    count -= 2;
-    result.textContent = count
-}
-
-button1.addEventListener("click", countUp)
-button2.addEventListener("click", countDown)
+mainBtn.onclick = pushFavoWord
